adb2fc818dfa3919a2707bcf298665c5fad8a0ef
Kenneth Shimabukuro <kenneth@dololo.io>
2021-04-15 17:08:38 +0200

    updated files

---
+++ script.js
@@ -0,0 +1,232 @@
+//0 alerts and console.log
+// one alert
+// one console log
+
+// hint
+// console.log("hello world");
+// your code here:
+
+
+
+//1 variables - var, let, const
+// create one var string (a sentence) i.e. var myName = "Kenneth";
+// create one let string (a sentence) 
+// create one let number
+
+// hint
+// strings have inverted commas -> let mySentence = "I am a sentence";
+// numbers do not, just declare the number -> let myNumber = 5;
+// your code here: 
+
+
+
+//2 datatypes - (4 main ones, string, number, boolean, null)
+
+// declare a string variable called name1 that can be changed 
+// declare a number as PI= 3.1415926535 that cannot be changed
+// try to change pi to something else and see what happens
+// declare a number myRandomNumber set it to 5
+// declare a boolean isLearning and set it to true
+// print any of them to the console
+// print the type of the boolean
+
+// hint
+// variables all have types, i.e. 
+// (var or let - can change value) or (const - cannot change value)
+// remember var is sometimes dangerous to use, so use let
+// to get type use typeof variableName 
+// your code here: 
+
+
+
+//3 if else statements
+// create a boolean called switchIsOn and set it to true
+// create an if else statement
+//      if switchIsOn print out "lights are on"
+//      else print out "lights are off"
+
+// hint
+// syntax (the way the code looks)
+// if(condition){
+//     statement;
+// }else{
+//     statement;
+// }
+// your code here: 
+
+
+
+// TASK - for more experienced programmers
+//       if you're up for the task then try and solve this
+// first prime numbers in array generation>
+// https://www.codewars.com/kata/535bfa2ccdbf509be8000113/train/javascript
+
+
+
+// 4 arithmetic operators
+
+// concatenate "this is" and " a sentence" and print them out
+// get the remainder when 11 is divided by 5
+// make a variable a = 1, incerement it and print out the number one above a
+// get 5 to the power of 2
+
+// hint
+// concatentaion works like adding two numbers together
+// remainder is found with % (modulo)
+// to increment a number use ++ , to decrement a number use -- (order is NB)
+// exponentiation is done with **
+// your code here: 
+
+
+
+//5 comparison operators
+
+// create variable a , set to 5 and variable b set to "5"
+// print a == b then print a === b see the difference? Think about why
+
+// create variable c = 6 and d = "6"
+// what do you expect c != d to equal? Think about this before printing it
+// your code here: 
+
+
+
+//6 logical operators
+
+/* 
+Coding Challenge #1
+Mark and John are trying to compare their BMI (Body Mass Index), 
+which is calculated using the formula:
+BMI = mass / height ** 2 = mass or (height * height) (mass in kg and height in meters).
+
+Your tasks:
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using either formula
+3. Create a Boolean variable 'markHigherBMI' containing information about
+whether Mark has a higher BMI than John.
+4. print about whether Mark's BMI is heigher than John's BMI
+Test data:
+-   Data 1: Marks weights 78 kg and is 1.69 m tall. 
+    John weights 92 kg and is 1.95 m tall.
+-   Data 2: Marks weights 95 kg and is 1.88 m tall. 
+    John weights 85 kg and is 1.76 m tall.
+
+*/
+//your code here:
+
+
+
+//8 arrays
+// create list of fruit with items "pear", "apple", "banana"
+// print pear
+// take banana out of the list
+// now add a fruit of your own
+// print array
+
+// hints
+// use array indexing [] i.e. myfruit[index]
+// use built-in array methods/functions push(), pop()
+// final array should be
+// your code here: 
+
+
+//9 objects
+// uncomment code below
+// let movie = {
+// title: "Avengers",
+//     rating: 9.5,
+//         characters: ["iron man", "black widow", "hulk"]
+
+// }
+// how would we add a character to the movie? Try it
+// your code here: 
+
+
+
+
+//10 functions
+// create a function that prints out the numbers one to ten
+
+/*
+Coding Challenge #2
+printing the first ten odd numbers starting from 1
+
+By definition, 
+an odd number is an integer that can be written in the form 2k + 1, 
+for some integer k. This means we can write x = 2k + 1, 
+where k is some integer (a number which is not a fraction; a whole number)
+
+Your task: write a function that prints out ten odd numbers starting from 1
+*/
+// your code here: 
+
+
+
+//extras
+// why var is bad
+
+// var x = 1;
+
+//x scoped to this if statement?
+// if (x === 1) { 
+//     var x = 5;
+
+//     console.log(x);
+//     // expected output: 2
+// }
+// x should only be changed inside the function (now change to let)
+
+
+//11 array of objects (StackOverflow)
+// first write this
+// let todoList = [
+//     {
+//         id: 1,
+//         title: "take out trash",
+//         isCompleted = true
+//     },
+//     {
+//         id: 2,
+//         title: "feed dogs",
+//         isCompleted =  true
+//     },
+//     {
+//         id: 3,
+//         title: "go to work",
+//         isCompleted = false
+//     }
+// ];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/
